68ea4044253f518266fbb936b5bccabbfef7a9af
小生不才 <66526413+694340796@users.noreply.github.com>
2025-06-19 18:00:59 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: '小生聚合TV',
+    url: 'https://小生聚合TV.is-an.org',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
